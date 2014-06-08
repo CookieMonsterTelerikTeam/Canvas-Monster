8621a0ce4d65225f23510bebef3880ae32104d93
Lyubomir Mitkov <lyubomir.mitkov@gmail.com>
2014-06-09 01:18:08 +0300

    Added Input Image Function
    Added a function to put an image from a URL to the canvas.

--- CanvasMonster/scripts/drawJS.js
+++ CanvasMonster/scripts/drawJS.js
@@ -2,6 +2,9 @@
 var onDraw = false;
 var color = 'black';
 var brushWidth = 10;
+var isHelpDivOn = false;
+var isRubberOn = false;
+var rubberPreviousColor;
 
 canvas = document.getElementById('the-canvas');
 context = canvas.getContext('2d');
@@ -70,8 +73,20 @@ function ev_canvas(ev) {
 function toggleDraw() {
     onDraw = !onDraw;
     var btn = document.getElementById('brush');
-    btn.style.color = (onDraw) ? "white" : "yellowgreen";
-    btn.style.background = (onDraw) ? "yellowgreen" : "white";
+    document.body.style.cursor = (onDraw) ? 'crosshair' : 'default';
+    changeButtonToPressed(btn, onDraw);
+}
+
+//TODO - Rubber Mode (when on 1) gets the old color 2) sets the main color to white;  when off puts the old color back)
+function toggleRubber() {
+    if (isRubberOn = false) {
+        rubberPreviousColor = document.getElementById('color-button').value;
+        document.getElementById('color-button').value = 'white';
+        isRubberOn = true;
+    } else {
+        document.getElementById('color-button').value = rubberPreviousColor;
+        isRubberOn = false;
+    }
 }
 
 //Change current color
@@ -88,4 +103,67 @@ function changeWidth() {
 //Clear canvas
 function clearCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+//Adds to the DOM help div to enter the url of the image. 
+function showImageInput() {
+    var parent = document.getElementById('canvas-container');
+    var div = document.createElement('div');
+    var btn = document.getElementById('image-input');
+
+    //Check if a help div is added not to add another one
+    if (!isHelpDivOn) {
+        div.id = 'temp-div';
+        div.innerHTML = '<label for="img-url-input" style="font-size:19px; color:white;">Enter URL of the image here: </label>\
+        <input type="text" id="img-url-input" style="width:300px; height:25px"/>\
+        <button class="btn" id="img-url" onclick="inputPicture()">Input</button>';
+
+        parent.parentNode.insertBefore(div, parent.nextSibling);
+    }
+
+    isHelpDivOn = true;
+    changeButtonToPressed(btn, isHelpDivOn);
+}
+
+//Input picture P.S. Tryed to input images from the local computer but there is a bug on the chrome
+//and it doesn't allow it
+function inputPicture() {
+    var picPath = document.getElementById('img-url-input').value;
+    var image = new Image();
+    image.src = picPath;
+
+    //Check and configure if needed the imported image size
+    image.onload = function () {
+        if (image.width > canvas.width || image.height > canvas.height) {
+            var width = image.width,
+                height = image.height;
+
+            if (image.width > canvas.width) {
+                width = canvas.width;
+            }
+            if (image.height > canvas.height) {
+                height = canvas.height;
+            }
+            context.drawImage(image, 0, 0, width, height);
+        } else {
+            context.drawImage(image, 0, 0);
+        }
+
+
+    };
+
+    //Remove the helper div
+    var divToRemove = document.getElementById('temp-div');
+    divToRemove.outerHTML = "";
+
+    //Change color of the button to untoggled
+    var btn = document.getElementById('image-input');
+    isHelpDivOn = false;
+    changeButtonToPressed(btn, isHelpDivOn);
+}
+
+function changeButtonToPressed(btn, condition) {
+    btn.style.color = (condition) ? "white" : "yellowgreen";
+    btn.style.background = (condition) ? "yellowgreen" : "white";
 }
